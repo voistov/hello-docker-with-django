cfa9a80bedf32deaffd87ff862b66187eab45a19
voistov <voistov@ukr.net>
2022-07-04 17:02:56 +0300

    v5

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse('Hello, World from Valeriy v4!')
+    return HttpResponse('Hello, World from Valeriy v5!')
